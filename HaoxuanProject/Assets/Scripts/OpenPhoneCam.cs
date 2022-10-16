9a685eed393bf25dbb36a3d0644b453c9615eb5d
MinPan9585 <minpan9585@gmail.com>
2022-10-16 21:54:46 +0800

    Course1016

---
+++ HaoxuanProject/Assets/Scripts/OpenPhoneCam.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OpenPhoneCam : MonoBehaviour
+{
+    public RawImage cameraImage;
+    private WebCamTexture camTexture;
+    public SpriteRenderer square;
+    private void Start()
+    {
+        StartCoroutine(OpenPhoneCamera());
+    }
+    /// <summary>
+    /// 打开手机摄像头
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator OpenPhoneCamera()
+    {
+        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);//请求打开手机摄像头权限,手机端会弹出一个选择弹窗
+        WebCamDevice[] devices = WebCamTexture.devices;
+        camTexture = new WebCamTexture(devices[0].name,Screen.width,Screen.height);
+        camTexture.Play();
+        cameraImage.texture = camTexture;
+        //square.
+    }
+}
---
+++ HaoxuanProject/Assets/Scripts/SwitchToInvest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SwitchToInvest : MonoBehaviour
+{
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(SwitchScene);
+    }
+
+    void SwitchScene(){
+        SceneManager.LoadScene("InvestigationScene1016");
+    }
+}
