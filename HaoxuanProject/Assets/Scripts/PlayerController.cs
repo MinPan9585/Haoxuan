11620e247d616f14eb0d69e5ad7aefd42033f8e9
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2022-10-17 23:25:12 +0800

    Add Input Actions and Callback Functions

---
+++ HaoxuanProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool pressHeld = false;
+    public void SingleHit(InputAction.CallbackContext context)
+    {
+        //查找附近小敌人tag的object，执行销毁，播放特效，音效，得分（距离判断）
+        if (context.phase == InputActionPhase.Performed)
+        {
+            //执行攻击
+            Debug.Log("you hit screen once...");
+        }
+    }
+
+    public void LongPress(InputAction.CallbackContext context)
+    {
+        //大敌人
+        if (context.performed)
+        {
+            Debug.Log("you long pressed screen once");
+            pressHeld = true;
+        }
+        if (context.canceled && pressHeld)
+        {
+            //执行攻击
+            pressHeld = false;
+        }
+    }
+}
