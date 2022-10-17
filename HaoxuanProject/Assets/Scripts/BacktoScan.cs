1b35531eafb2b0317367d0b936bb5658a46546ea
LeonhartYan <yanhaoxuan2004@foxmail.com>
2022-10-17 10:35:16 +0800

    1016
    ui, font

---
+++ HaoxuanProject/Assets/Scripts/BacktoScan.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BacktoScan : MonoBehaviour
+{
+    void Start(){
+        this.GetComponent<Button>().onClick.AddListener(SwitchScene);
+    }
+
+    void SwitchScene(){
+        SceneManager.LoadScene("MainScene1016");
+    }
+}
